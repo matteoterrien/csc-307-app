c8d797eb92a6519bc4de01600f0f8340a20b4532
Matteo Terrien <matteo.terrien@gmail.com>
2024-04-15 09:24:24 -0700

    implemented REST API

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,8 +35,20 @@ const users = {
 
 app.use(express.json());
 
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
+};
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
 };
 
 app.get("/users", (req, res) => {
@@ -50,6 +62,18 @@ app.get("/users", (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
