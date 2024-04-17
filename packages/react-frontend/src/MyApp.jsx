4ebdcbc4373e1ded05f6eac89977aea342c845af
Matteo Terrien <matteo.terrien@gmail.com>
2024-04-17 09:50:18 -0700

    POST working on Backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -5,13 +5,29 @@ import Form from "./Form";
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
     return promise;
   }
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   function removeOneCharacter(index) {
